a94fd55307f691e011c608d38148c742a0dea58c
signoiidx <signo.iidx@gmail.com>
2021-01-07 06:04:32 +0900

    initial commit

---
+++ ieee_pdf_renamer.py
@@ -0,0 +1,32 @@
+import os
+import re
+import requests
+import bs4
+
+# make list of the files in the current directory
+dir_files = str(os.listdir(os.getcwd()))
+
+# search list for IEEE PDF files
+pdf_files = re.findall(r'[0-9]{8}.pdf', dir_files)
+pdf_nums = [pdf_files.replace(".pdf", "") for pdf_files in pdf_files]
+
+if len(pdf_files) > 0:
+	# gen URL for accessing IEEE Xplore, scrape title data, and rename file
+	for num, old_path in zip(pdf_nums, pdf_files):
+		# gen URL for accessing IEEE Xplore
+		num_url = num.lstrip('0')
+		access_URL = 'https://ieeexplore.ieee.org/document/' + num_url
+		# scrape title data
+		get_url_info = requests.get(access_URL)
+		soup = bs4.BeautifulSoup(get_url_info.text, 'html.parser')
+		webtitle = soup.title.text
+		title = re.sub(r' - IEEE.*', "", webtitle) # remove journal title
+		# genarate new filename
+		title2 = re.sub(r'[\/:,;*?"<>|]', "", title) # del f***ing char
+		new_path = re.sub(r' ', "_", title2) + ".pdf" # add underbar
+		# rename
+		os.rename(old_path, new_path)
+		# plot result
+		print("Done. \"" + old_path + "\" -> \"" + new_path + "\"")
+else : # Not Found
+	print("No PDF files detected.")
---
+++ readme.md
@@ -0,0 +1,19 @@
+IEEE PDF Renamer
+======
+**IEEE PDF Renamer** is a script for rename PDF downloaded from IEEE Xplore.
+
+## Usage
+- place ```ieee_pdf_renamer.py``` on desired folder.
+- execute
+```
+$ python ieee_pdf_renamer.py
+Done. "12345678.pdf" -> "The_Example_of_Renaming_PDF_from_IEEE.pdf"
+```
+- script rename all 8-digit-titled PDF to correct title in current folder.
+
+## References
+- [Pythonでカレントディレクトリを取得、変更（移動）](https://note.nkmk.me/python-os-getcwd-chdir/)
+- [Pythonで文字列のリスト（配列）の条件を満たす要素を抽出、置換](https://note.nkmk.me/python-list-str-select-replace/)
+- [［Python入門］リストと繰り返し処理 (1/3)](https://www.atmarkit.co.jp/ait/articles/1906/11/news007.html)
+- [PythonでWebページ情報を取得してみる](https://qiita.com/__init__/items/d53a281ef757b22f4732)
+- [Pythonのfor文によるループ処理（range, enumerate, zipなど）](https://note.nkmk.me/python-for-usage/)
